ed42a664b2131e5d9eb42321f7b16b7acc60395e
Junior <turatsinzejunior83@gmail.com>
2019-02-22 14:00:02 -0800

    first commit

---
+++ MovieApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MovieApp.NewDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MovieContext())
+            {
+                db.Movies.Add(new Movietbl { title = "Vimpire diaries" });
+                db.Movies.Add(new Movietbl { year = "2019" });
+                db.Movies.Add(new Movietbl { category_name = "horror Movie" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var Movietbl in db.Movies)
+                {
+                    Console.WriteLine(" - {0}", Movietbl.title, Movietbl.year, Movietbl.category_name);
+                }
+            }
+        }
+    }
+}
